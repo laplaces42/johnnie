30840faaed128be54b032797c5435f3641e56c62
LaPlace Sallis IV <laplace.sallis@gmail.com>
2024-12-27 18:45:37 -0500

    Updated text for about page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,4 +1,5 @@
 import "../stylesheets/AboutPage.css";
+import { motion } from "framer-motion";
 
 export function AboutPage() {
   return (
@@ -74,24 +75,40 @@ export function AboutPage() {
     //   </div>
     // </div>
     <div className="ab">
-      <div className="ab1">
+      <motion.div
+        className="ab1"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ amount: 0.4, once: true }}
+        transition={{ duration: 0.8 }}
+      >
         <h1>About Johnnie</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, iste
-          aspernatur suscipit provident odit veniam ipsa? Corporis
-          exercitationem commodi dignissimos?
+          Johnnie is a startup designed to change the grant writing and grant
+          searching process for nonprofits everywhere.
         </p>
-      </div>
-      <div className="ab2">
+      </motion.div>
+      <motion.div
+        className="ab2"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ amount: 0.4, once: true }}
+        transition={{ duration: 0.8 }}
+      >
         <h2>Our Mission</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem ab
-          natus, itaque provident magni deleniti voluptas nam repellat rem ipsum
-          doloribus quasi facilis beatae aspernatur vero distinctio ullam porro
-          iste.
+          At Johnnie, we aim to simplify the grant application process, empower
+          organizations, and provide unparalleled access to professional
+          grant-writing services.
         </p>
-      </div>
-      <div className="ab3">
+      </motion.div>
+      <motion.div
+        className="ab3"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ amount: 0.4, once: true }}
+        transition={{ duration: 0.8 }}
+      >
         <h2>Meet the Founders</h2>
         <div className="ab3-founders">
           <div className="ab3-founder1">
@@ -110,47 +127,60 @@ export function AboutPage() {
             <img src="" alt="" />
             <h3>LaPlace Sallis IV</h3>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos
-              aspernatur doloremque sint odit ipsum quas id possimus iste optio
-              nihil alias magni non laborum, natus ducimus sequi labore hic!
-              Iure quos perferendis ratione, harum mollitia voluptatum ea
-              praesentium natus placeat dolores, saepe expedita ab dolor. Porro
-              rem suscipit error facilis!
+              LaPlace is a Rice University undergraduate student currently
+              studying Computer Science and Business. With a strong foundation
+              in programming and machine learning, he is passionate about
+              leveraging technology to drive innovation. At the heart of
+              LaPlace's vision for the startup is the desire to empower
+              volunteer fire departments through accessible, user-friendly
+              tools.
             </p>
           </div>
         </div>
-      </div>
-      <div className="ab4">
+      </motion.div>
+      <motion.div
+        className="ab4"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ amount: 0.4, once: true }}
+        transition={{ duration: 0.8 }}
+      >
         <h2>Our Story</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          consectetur distinctio quisquam maxime veniam dolorem. Praesentium
-          voluptates nobis distinctio ab repudiandae, asperiores id eum, eius
-          quisquam labore voluptatem, accusamus facilis explicabo omnis ullam
-          suscipit doloremque aspernatur fugit minus molestiae saepe. Ea
-          incidunt ut enim iure pariatur eos, corrupti nobis ullam cupiditate
-          aliquam sit, ipsum ab quidem obcaecati provident. Et natus alias
-          fugiat tempore. Veritatis optio obcaecati natus, aliquam harum in
-          illum iure, sit, id velit aspernatur aliquid? Deserunt eos assumenda
-          dolorem odio iste beatae facilis aut velit, nemo laudantium inventore
-          rem quibusdam! Neque doloremque praesentium velit ut aspernatur! In
-          magni temporibus atque! Optio nemo nulla nesciunt error, neque
-          sapiente recusandae corporis velit laudantium nihil enim hic maiores
-          cum. Consequatur quos natus, voluptatibus neque aperiam explicabo
-          nihil laborum cum repellat quo ipsum placeat unde ratione possimus
-          vero ipsa? Dolores, doloribus? Officiis laboriosam sed quos, iusto
-          repudiandae tempore voluptate consectetur nobis ullam consequuntur,
-          eos error eveniet eum voluptatum quae neque sint ab esse blanditiis
-          aperiam commodi perferendis. Inventore fuga provident accusamus nam
-          enim illo, et maiores sint iure recusandae reprehenderit, porro
-          consequuntur ducimus libero temporibus! Aliquam explicabo possimus
-          harum quas repudiandae repellat fugit molestias, sapiente expedita
-          dolor, dolores ratione quia. Repellendus, eligendi!
+          Volunteer fire departments are the unsung heroes of our communities,
+          dedicating their time and risking their lives to protect others. Yet,
+          despite their critical role, many face significant challenges, from
+          retention and recruitment struggles to technological gaps and limited
+          funding.
         </p>
-      </div>
-      <div className="ab5">
+        <p>
+          In 2024, Johnnie was born out of a mission to change this narrative.
+          During in-depth conversations with volunteer fire departments, we
+          identified that a lack of funding often compounds other issues they
+          face. Without adequate resources, departments struggle to invest in
+          recruitment, retention, and improved communication tools.
+        </p>
+        <p>
+          At Johnnie, we are determined to tackle this funding gap head-on. By
+          simplifying grant searches and streamlining the grant-writing process,
+          we aim to empower fire departments with the tools they need to secure
+          essential resources and continue their invaluable work.
+        </p>
+        <p>
+          Our journey is just beginning, but our commitment to supporting these
+          everyday heroes remains steadfast. Together, we can build stronger,
+          better-equipped fire departments for the future.
+        </p>
+      </motion.div>
+      <motion.div
+        className="ab5"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ amount: 0.4, once: true }}
+        transition={{ duration: 0.8 }}
+      >
         <h2>Contact Us</h2>
-      </div>
+      </motion.div>
     </div>
   );
 }
