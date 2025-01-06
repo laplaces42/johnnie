5f71c4f7c0ff4cd3541e4a5b11ef2cc5cb0c058a
ishaaniwani <ishaansinha155@gmail.com>
2025-01-06 13:07:31 -0800

    color scheme choices

--- frontend/src/components/MenuBar.jsx
+++ frontend/src/components/MenuBar.jsx
@@ -6,7 +6,7 @@ export function MenuBar() {
     <div className="menu-bar">
       <div>
         <button onClick={() => navigate("/")} className="menu-bar-title-logo">
-          <img className="logo" src="/images/logo.png" alt="" />
+          <img className="logo" src="/images/logo_white.png" alt="" />
           <p className="menu-bar-title">Johnnie</p>
         </button>
       </div>
