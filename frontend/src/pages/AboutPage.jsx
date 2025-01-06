bede04fae979e89ee166ef2dc6fa6eb7ce58d25a
LaPlace Sallis IV <laplace.sallis@gmail.com>
2025-01-05 21:49:09 -0500

    Created color scheme for about page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -75,40 +75,22 @@ export function AboutPage() {
     //   </div>
     // </div>
     <div className="ab">
-      <motion.div
-        className="ab1"
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ amount: 0.4, once: true }}
-        transition={{ duration: 0.8 }}
-      >
+      <div className="ab1">
         <h1>About Johnnie</h1>
         <p>
           Johnnie is a startup designed to change the grant writing and grant
           searching process for nonprofits everywhere.
         </p>
-      </motion.div>
-      <motion.div
-        className="ab2"
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ amount: 0.4, once: true }}
-        transition={{ duration: 0.8 }}
-      >
+      </div>
+      <div className="ab2">
         <h2>Our Mission</h2>
         <p>
           At Johnnie, we aim to simplify the grant application process, empower
           organizations, and provide unparalleled access to professional
           grant-writing services.
         </p>
-      </motion.div>
-      <motion.div
-        className="ab3"
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ amount: 0.4, once: true }}
-        transition={{ duration: 0.8 }}
-      >
+      </div>
+      <div className="ab3">
         <h2>Meet the Founders</h2>
         <div className="ab3-founders">
           <div className="ab3-founder1">
@@ -116,10 +98,10 @@ export function AboutPage() {
             <h3>Ishaan Sinha</h3>
             <p>
               Ishaan is a Rice University undergraduate student studying
-              Electrical and Computer Engineering. He is passionate about 
+              Electrical and Computer Engineering. He is passionate about
               user-centered design and making sure that this service truly helps
-              volunteer fire departments. Ishaan values the relationships and community
-              so strong in this space to overcome key challenges.
+              volunteer fire departments. Ishaan values the relationships and
+              community so strong in this space to overcome key challenges.
             </p>
           </div>
           <div className="ab3-founder2">
@@ -136,14 +118,8 @@ export function AboutPage() {
             </p>
           </div>
         </div>
-      </motion.div>
-      <motion.div
-        className="ab4"
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ amount: 0.4, once: true }}
-        transition={{ duration: 0.8 }}
-      >
+      </div>
+      <div className="ab4">
         <h2>Our Story</h2>
         <p>
           Volunteer fire departments are the unsung heroes of our communities,
@@ -170,20 +146,124 @@ export function AboutPage() {
           everyday heroes remains steadfast. Together, we can build stronger,
           better-equipped fire departments for the future.
         </p>
-      </motion.div>
-      <motion.div
-        className="ab5"
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ amount: 0.4, once: true }}
-        transition={{ duration: 0.8 }}
-      >
+      </div>
+      <div className="ab5">
         <h2>Contact Us</h2>
         <p>
           Want to get in touch? Feel free to email us at team@johnniegrants.com!
         </p>
-      </motion.div>
+      </div>
     </div>
+    // <div className="ab">
+    //   <motion.div
+    //     className="ab1"
+    //     initial={{ opacity: 0, y: 50 }}
+    //     whileInView={{ opacity: 1, y: 0 }}
+    //     viewport={{ amount: 0.4, once: true }}
+    //     transition={{ duration: 0.8 }}
+    //   >
+    //     <h1>About Johnnie</h1>
+    //     <p>
+    //       Johnnie is a startup designed to change the grant writing and grant
+    //       searching process for nonprofits everywhere.
+    //     </p>
+    //   </motion.div>
+    //   <motion.div
+    //     className="ab2"
+    //     initial={{ opacity: 0, y: 50 }}
+    //     whileInView={{ opacity: 1, y: 0 }}
+    //     viewport={{ amount: 0.4, once: true }}
+    //     transition={{ duration: 0.8 }}
+    //   >
+    //     <h2>Our Mission</h2>
+    //     <p>
+    //       At Johnnie, we aim to simplify the grant application process, empower
+    //       organizations, and provide unparalleled access to professional
+    //       grant-writing services.
+    //     </p>
+    //   </motion.div>
+    //   <motion.div
+    //     className="ab3"
+    //     initial={{ opacity: 0, y: 50 }}
+    //     whileInView={{ opacity: 1, y: 0 }}
+    //     viewport={{ amount: 0.4, once: true }}
+    //     transition={{ duration: 0.8 }}
+    //   >
+    //     <h2>Meet the Founders</h2>
+    //     <div className="ab3-founders">
+    //       <div className="ab3-founder1">
+    //         <img src="" alt="" />
+    //         <h3>Ishaan Sinha</h3>
+    //         <p>
+    //           Ishaan is a Rice University undergraduate student studying
+    //           Electrical and Computer Engineering. He is passionate about
+    //           user-centered design and making sure that this service truly helps
+    //           volunteer fire departments. Ishaan values the relationships and community
+    //           so strong in this space to overcome key challenges.
+    //         </p>
+    //       </div>
+    //       <div className="ab3-founder2">
+    //         <img src="" alt="" />
+    //         <h3>LaPlace Sallis IV</h3>
+    //         <p>
+    //           LaPlace is a Rice University undergraduate student currently
+    //           studying Computer Science and Business. With a strong foundation
+    //           in programming and machine learning, he is passionate about
+    //           leveraging technology to drive innovation. At the heart of
+    //           LaPlace's vision for the startup is the desire to empower
+    //           volunteer fire departments through accessible, user-friendly
+    //           tools.
+    //         </p>
+    //       </div>
+    //     </div>
+    //   </motion.div>
+    //   <motion.div
+    //     className="ab4"
+    //     initial={{ opacity: 0, y: 50 }}
+    //     whileInView={{ opacity: 1, y: 0 }}
+    //     viewport={{ amount: 0.4, once: true }}
+    //     transition={{ duration: 0.8 }}
+    //   >
+    //     <h2>Our Story</h2>
+    //     <p>
+    //       Volunteer fire departments are the unsung heroes of our communities,
+    //       dedicating their time and risking their lives to protect others. Yet,
+    //       despite their critical role, many face significant challenges, from
+    //       retention and recruitment struggles to technological gaps and limited
+    //       funding.
+    //     </p>
+    //     <p>
+    //       In 2024, Johnnie was born out of a mission to change this narrative.
+    //       During in-depth conversations with volunteer fire departments, we
+    //       identified that a lack of funding often compounds other issues they
+    //       face. Without adequate resources, departments struggle to invest in
+    //       recruitment, retention, and improved communication tools.
+    //     </p>
+    //     <p>
+    //       At Johnnie, we are determined to tackle this funding gap head-on. By
+    //       simplifying grant searches and streamlining the grant-writing process,
+    //       we aim to empower fire departments with the tools they need to secure
+    //       essential resources and continue their invaluable work.
+    //     </p>
+    //     <p>
+    //       Our journey is just beginning, but our commitment to supporting these
+    //       everyday heroes remains steadfast. Together, we can build stronger,
+    //       better-equipped fire departments for the future.
+    //     </p>
+    //   </motion.div>
+    //   <motion.div
+    //     className="ab5"
+    //     initial={{ opacity: 0, y: 50 }}
+    //     whileInView={{ opacity: 1, y: 0 }}
+    //     viewport={{ amount: 0.4, once: true }}
+    //     transition={{ duration: 0.8 }}
+    //   >
+    //     <h2>Contact Us</h2>
+    //     <p>
+    //       Want to get in touch? Feel free to email us at team@johnniegrants.com!
+    //     </p>
+    //   </motion.div>
+    // </div>
   );
 }
 
--- frontend/src/pages/PricingAndFAQPage.jsx
+++ frontend/src/pages/PricingAndFAQPage.jsx
@@ -5,24 +5,23 @@ export function PricingAndFAQPage() {
   return (
     <div className="pricing-page">
       <div className="cp">
-            <div className="ab4">
-                <h2>Pricing</h2>
-                <p>
-                    Our prices varies on depending on the funding opportunity. To get an accurate estimate for our pricing, complete this quick quote estimator!
-                </p>
-                <div className="typeform-wrapper">
-                    <iframe 
-                        src="https://maxzge0ttlh.typeform.com/to/sehN5JTT" 
-                        className="typeform-iframe"
-                        title="Typeform"
-                    ></iframe>
-                </div>
-                <h4> 2</h4>
-            </div>
+        <h2>Pricing</h2>
+        <p>
+          Our prices varies on depending on the funding opportunity. To get an
+          accurate estimate for our pricing, complete this quick quote
+          estimator!
+        </p>
+        <div className="typeform-wrapper">
+          <iframe
+            src="https://maxzge0ttlh.typeform.com/to/sehN5JTT"
+            className="typeform-iframe"
+            title="Typeform"
+          ></iframe>
+        </div>
+        <h4> 2</h4>
       </div>
       <FAQBar />
     </div>
-    
   );
 }
 
