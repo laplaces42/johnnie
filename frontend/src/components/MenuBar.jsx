7f2cfff7ff7485aedcce94c192ba170d5132519f
LaPlace Sallis IV <laplace.sallis@gmail.com>
2025-01-19 12:14:11 -0600

    Added mobile compatibility for top menu bar

--- frontend/src/components/MenuBar.jsx
+++ frontend/src/components/MenuBar.jsx
@@ -1,7 +1,11 @@
 import "../stylesheets/MenuBar.css";
 import { useNavigate, useParams } from "react-router-dom";
+import { useState } from "react";
+
 export function MenuBar() {
   const navigate = useNavigate();
+  const [menuVisible, setMenuVisible] = useState(false);
+
   return (
     <div className="menu-bar">
       <div>
@@ -10,6 +14,43 @@ export function MenuBar() {
           <p className="menu-bar-title">Johnnie</p>
         </button>
       </div>
+      <div className="burger-menu">
+        <img
+          className="burger-icon"
+          onClick={() => setMenuVisible((menuVisible) => !menuVisible)}
+          src="/images/hamburger.png"
+          alt=""
+        />
+        <div className={`burger-buttons${menuVisible ? " active" : ""}`}>
+          <button
+            onClick={() => {
+              navigate("/pricing");
+              setMenuVisible((menuVisible) => !menuVisible);
+            }}
+            className="burger-button"
+          >
+            Pricing and FAQ
+          </button>
+          <button
+            onClick={() => {
+              navigate("/about");
+              setMenuVisible((menuVisible) => !menuVisible);
+            }}
+            className="burger-button"
+          >
+            About Johnnie
+          </button>
+          <button
+            onClick={() => {
+              navigate("/contact");
+              setMenuVisible((menuVisible) => !menuVisible);
+            }}
+            className="burger-button"
+          >
+            Get Started
+          </button>
+        </div>
+      </div>
       <div className="menu-bar-buttons">
         {/* <button onClick={() => navigate("/search")} className="menu-bar-button">
           Search Grants
@@ -17,7 +58,10 @@ export function MenuBar() {
         <button onClick={() => navigate("/hire")} className="menu-bar-button">
           Hire a Grant Writer
         </button> */}
-        <button onClick={() => navigate("/pricing")} className="menu-bar-button">
+        <button
+          onClick={() => navigate("/pricing")}
+          className="menu-bar-button"
+        >
           Pricing and FAQ
         </button>
         <button onClick={() => navigate("/about")} className="menu-bar-button">
