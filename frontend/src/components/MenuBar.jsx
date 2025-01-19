449466692fef6c93d0faec83d59eea456d47ee6a
LaPlace Sallis IV <laplace.sallis@gmail.com>
2025-01-19 13:09:23 -0600

    Added outside click functionality for hamburger menu

--- frontend/src/components/MenuBar.jsx
+++ frontend/src/components/MenuBar.jsx
@@ -1,10 +1,26 @@
 import "../stylesheets/MenuBar.css";
 import { useNavigate, useParams } from "react-router-dom";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export function MenuBar() {
   const navigate = useNavigate();
   const [menuVisible, setMenuVisible] = useState(false);
+
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenuVisible(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="menu-bar">
@@ -14,7 +30,7 @@ export function MenuBar() {
           <p className="menu-bar-title">Johnnie</p>
         </button>
       </div>
-      <div className="burger-menu">
+      <div ref={menuRef} className="burger-menu">
         <img
           className="burger-icon"
           onClick={() => setMenuVisible((menuVisible) => !menuVisible)}
